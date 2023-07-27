884b3490009bbb3c42b7caa971dbd58e9d3760f1
aktrooper9 <54760319+OceanNut420@users.noreply.github.com>
2023-07-26 18:05:43 -0700

    initial

---
+++ src/main/java/com/bookApi/BookAPI/presentation/HomeController.java
@@ -0,0 +1,15 @@
+package com.bookApi.BookAPI.presentation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.MOVED_PERMANENTLY)
+    public String home() {
+        return "redirect:swagger-ui.html";
+    }
+}
