a865359000e2a5f4f77bf57b15458e275c917267
aktrooper9 <andrewkim.bc@gmail.com>
2023-09-30 11:26:04 -0700

    test methods begin working on front end

--- src/main/java/com/bookApi/BookAPI/presentation/LocController.java
+++ src/main/java/com/bookApi/BookAPI/presentation/LocController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.bookApi.BookAPI.service.LocService;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 public class LocController {
     private final LocService locService;
---
+++ src/test/java/presentation/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package presentation;
+import com.bookApi.BookAPI.presentation.HomeController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class HomeControllerTest {
+    private HomeController homeController;
+
+    @BeforeEach
+    void setUp() {
+        homeController = new HomeController();
+    }
+
+    @Test
+    void whenHome_thenReturnRedirect() {
+        //given
+        String expected = "redirect:swagger-ui.html";
+
+        //when
+        String actual = homeController.home();
+
+        //then
+        assertEquals(expected, actual);
+    }
+}
